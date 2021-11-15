4dc000471495ddee195694dc284722432c3b0270
enjee1 <22105805+enjee1@users.noreply.github.com>
2021-11-15 15:20:49 -0500

    Initial commit

---
+++ src/test/java/com/careerdevs/Multi_API/MultiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.Multi_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
